8569181d568c975527dd208ce5d329f965ec13a8
Laurier Loiselle <lal@xiphos.com>
2021-10-19 14:41:44 -0400

    compute error count valid when no_tx or no_rx
    When no tx or no rx parameters are set, the returned count will saturate
    because the tx or rx value will be 0. This makes the returned value
    useless to check directly if there was a transmission error.
    
    In that case, what needs to be checked is the error count, which informs
    the application on reception error.
    
    When doing only tx, the return should always be 0, since there is no
    reception error in that case. We didn't add a specific case for this
    to keep the change less verbose.

--- linux-serial-test.c
+++ linux-serial-test.c
@@ -616,6 +616,18 @@ static int diff_ms(const struct timespec *t1, const struct timespec *t2)
 	return (diff.tv_sec * 1000 + diff.tv_nsec/1000000);
 }
 
+static long long int compute_error_count()
+{
+	long long int result;
+	if (_cl_no_rx == 1 || _cl_no_tx == 1) {
+		result = _error_count;
+	} else {
+		result = llabs(_write_count - _read_count) + _error_count;
+	}
+
+	return (result > 125) ? 125 : (int)result;
+}
+
 int main(int argc, char * argv[])
 {
 	printf("Linux serial test app\n");
@@ -807,7 +819,5 @@ int main(int argc, char * argv[])
 	free(_cl_port);
 	free(_write_data);
 
-	long long int result = llabs(_write_count - _read_count) + _error_count;
-
-	return (result > 125) ? 125 : (int)result;
+	return compute_error_count();
 }
